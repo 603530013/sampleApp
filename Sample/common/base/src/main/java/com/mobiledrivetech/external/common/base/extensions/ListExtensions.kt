8ec988f819a8500994c1f362ccced6cce27c5ac8
WendyHsu <wendy.hsu@external.stellantis.com>
2024-02-22 11:14:07 +0800

    remove sample

--- Sample/common/base/src/main/java/com/mobiledrivetech/external/common/base/extensions/ListExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.mobiledrivetech.external.common.base.extensions
-
-inline fun <T> List<T>.indexOfFirstOrNull(predicate: (T) -> Boolean): Int? {
-    return indexOfFirst(predicate).takeIf { it != -1 }
-}
--- Sample/middleware/src/main/java/com/mobiledrivetech/external/middleware/model/SubError.kt
+++
@@ -1,3 +0,0 @@
-package com.mobiledrivetech.external.middleware.model
-
-data class SubError(val status: Int, val body: String)
